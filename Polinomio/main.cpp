b27bcf431a30709eb67239408de9f10c4794c756
macsebas <s.garcia@me.com>
2014-04-26 03:49:43 +0200

    Initial Commit

---
+++ Polinomio/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Polinomio
+//
+//  Created by Sebastián García on 26/04/14.
+//  Copyright (c) 2014 Sebastián García. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
